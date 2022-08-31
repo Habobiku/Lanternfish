2e8d0705f95eb37b31e5d2a9919b56dfde1efdbf
Habobiku <gireygame@gmail.com>
2022-08-31 19:23:48 +0300

    #

---
+++ LanternFish/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lanternfish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = 80;
+            List<int> lanternFish = new List<int> { 3, 4, 3, 1, 2 };
+            for (int i = 0; i < days; i++)
+            {
+                List<int> copyFish = new List<int>();
+                foreach (int fish in lanternFish)
+                {
+                    if (fish == 0)
+                    {
+                        copyFish.Add(8);
+                        copyFish.Add(6);
+                    }
+                    else
+                    {
+                        copyFish.Add(fish - 1);
+                    }
+                }
+                lanternFish = copyFish;
+            }
+
+            Console.WriteLine("The count of lanternfish after 80 days is " + lanternFish.Count);
+
+        }
+    }
+}
